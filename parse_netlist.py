9299f150d3930d47988803e49b198090edf06ec6
Jeff Chhen <56717962+jeffchhen@users.noreply.github.com>
2023-03-05 17:09:47 -0800

    Add files via upload
    Parse Netlist Python File. Outputs everything in a csv / pandas df. Gives total gates as well sum of all logic gates + time + iterations

--- parse_netlist.py
+++ parse_netlist.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 #!pip install git+https://github.com/circuitgraph/logiclocking
 #!pip install python-sat
 #!pip install scikit-learn
-
+import os
 import circuitgraph as cg
 from logiclocking import locks, write_key, read_key
 from logiclocking import attacks
@@ -26,7 +26,7 @@ def format_netlist(Random_netlist):
   first, rest = text.split(';',1)
   
   #instantiate headers for pandas
-  headers = "nan1 nan2 type name input1 input2 input3 input4 input5 input6 input7 input8 input9 input10\n"
+  headers = "nan1 nan2 type name input1 input2 input3 input4 input5 input6 input7 input8 input9 input10 i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i i ii i i i i i i i i i\n"
 
   netlist_extracted = headers + rest;
   # make a new file and write the rest
@@ -45,7 +45,7 @@ def parse_netlist_for_gate(df, name_of_file):
     df_values = df_values.rename(columns= { 'type':name_of_file})
     total_gates = df_values[name_of_file].sum();
     sum_row = pd.Series({name_of_file:total_gates}, name="total_gates");
-    df_values = df_values.append(sum_row)
+    df_values = pd.concat([df_values, sum_row.to_frame().T])
     df_values_T = df_values.T
     return df_values.T
 
@@ -92,4 +92,40 @@ df6= parse_netlist_for_gate(df,"MKR532xz_final_netlist.v");
 
 pieces = ( df2, df3, df5, df6, df7)
 df_final = pd.concat(pieces)
+df_final = pd.concat((df2, df3))
+
+df_main = pd.DataFrame()
+
+path = "D:\EEC289Q\\"
+file_head = "MKR532xz_final_netlist" 
+file_name = path + file_head+".v"
+c = cg.from_file(file_name)
+num_keys = 128
+lock_type = "mux"
+print(os.getcwd())
+for i in range(2):
+    #c1, k = locks.random_lut_lock(c, num_keys, lut_width = 4)
+    c1, k = locks.mux_lock(c, num_keys)
+    #c1, k = locks.xor_lock(c, num_keys)
+    #c1, k = locks.sfll_hd(c, num_keys, 1)
+    #c1, k = locks.tt_lock(c, num_keys)
+
+    #c1, k = locks.random_lut_lock(c, num_keys, lut_width=4)
+    result = attacks.miter_attack(c1, k, verbose=False, code_on_error=True)
+    cg.to_file(c1, file_head + lock_type + "_lock_trial" + str(i) + ".v")
+    df_temp = format_netlist(file_head + lock_type + "_lock_trial" + str(i) + ".v")
+    df6= parse_netlist_for_gate(df_temp, file_head + lock_type + "_lock_trial" + str(i) + ".v");
+    df6["time"] = result['Time']
+    df6["Iterations"] = result['Iterations']
+
+    df_main = pd.concat((df_main, df6))
+    if i == 1: 
+        df_temp = format_netlist(file_name)
+        df6= parse_netlist_for_gate(df_temp,file_head + lock_type + "no_lock" + str(i) + ".v");
+        df6["time"] = result['Time']
+        df6["Iterations"] = result['Iterations']
+        df_main = pd.concat((df_main, df6))
+    print("Trial " + str(i) + "Done")
+
+df_main.to_csv(path + "output_trial_data.txt",sep=',')
 
